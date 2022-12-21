4828cc870f8f51ca93b0893e438874da4283e26b
xltvy <68481602+xltvy@users.noreply.github.com>
2022-12-21 22:53:20 +0300

    initial app

---
+++ echo/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Home from './home';
+import TrackCard from './TrackCard';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      {/* <TrackCard /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ echo/src/TrackCard.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const TrackCard = (props) => {
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default TrackCard;
