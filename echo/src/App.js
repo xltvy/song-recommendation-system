cac8902898d4ad5baf09ddb726ac39fa7bc388b9
umutdenizsenerr <umutdeniz442010@gmail.com>
2022-12-21 23:45:54 +0300

    updateee

--- echo/src/App.js
+++ echo/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import React from 'react';
 import Home from './home';
 import TrackCard from './TrackCard';
-
+import SpotifyApi from './SpotifyApi';
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div className='App'>
+      {/* <Home /> */}
       {/* <TrackCard /> */}
+      <SpotifyApi />
     </div>
   );
 }
